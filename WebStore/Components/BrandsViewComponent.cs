ec072eef5a29c74c54ce90910efe41fd3561cd9e
ap-konovalov <a.konovalov@dodopizza.com>
2019-04-09 06:56:55 +0300

    Task 4. Edit left sidebar

---
+++ WebStore/Components/BrandsViewComponent.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.Entities;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.ViewModels;
+
+namespace WebStore.Components
+{
+    public class BrandsViewComponent : ViewComponent
+    {
+        private readonly IProductData _ProductData;
+
+        public BrandsViewComponent(IProductData ProductData)
+        {
+            _ProductData = ProductData;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var brands = GetBrands();
+            return View(brands);
+        }
+
+        private IEnumerable<BrandViewModel> GetBrands()
+        {
+            var brands = _ProductData.GetBrands();
+
+            return brands.Select(brand => new BrandViewModel(
+            {
+                Id = brand.Id,
+                Name = brand.Name,
+                Order = brand.Order
+            });
+        }
+    }
+}
