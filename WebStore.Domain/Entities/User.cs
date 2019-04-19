4750a5e568e58c201d978a1fd08621de4c47619d
ap-konovalov <a.konovalov@dodopizza.com>
2019-04-19 08:10:03 +0300

    Task 6. Identity role creatd

--- WebStore.Domain/Entities/User.cs
+++ WebStore.Domain/Entities/User.cs
@@ -4,7 +4,13 @@ namespace WebStore.Domain.Entities
 {
     public class User : IdentityUser
     {
+        public const string AdminUserName = "admin";
+        public const string DefaultAdminPassword = "admin";
         
+        public const string RoleAdmin = "Administrator";
+        public const string RoleUSer = "User";
+
+
     }
 
 //    public class Role : IdentityRole
--- WebStore/Data/WebStoreContextInitializer.cs
+++ WebStore/Data/WebStoreContextInitializer.cs
@@ -1,16 +1,22 @@
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using WebStore.DAL.Context;
+using WebStore.Domain.Entities;
 
 namespace WebStore.Data
 {
     public class WebStoreContextInitializer
     {
         private readonly WebStoreContext _db;
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
         
-        public WebStoreContextInitializer(WebStoreContext db)
+        public WebStoreContextInitializer(WebStoreContext db, UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
         {
             _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
         }
 
         public async Task InitializeAsync()
@@ -24,23 +30,11 @@ namespace WebStore.Data
 
             using (var transaction = _db.Database.BeginTransaction())
             {
-                _db.Sections.AddRange(TestData.Sections);
+                await _db.Sections.AddRangeAsync(TestData.Sections);
 
-                _db.Database.ExecuteSqlCommand("SET IDENTITY_INSERT [dbo].[Sections] ON");
-                _db.SaveChanges();
-                _db.Database.ExecuteSqlCommand("SET IDENTITY_INSERT [dbo].[Sections] OFF");
-
-                
-                transaction.Commit();
-            }
-            
-            using (var transaction = _db.Database.BeginTransaction())
-            {
-                _db.Brands.AddRange(TestData.Brands);
-
-                _db.Database.ExecuteSqlCommand("SET IDENTITY_INSERT [dbo].[Brands] ON");
-                _db.SaveChanges();
-                _db.Database.ExecuteSqlCommand("SET IDENTITY_INSERT [dbo].[Brands] OFF");
+                await _db.Database.ExecuteSqlCommandAsync("SET IDENTITY_INSERT [dbo].[Sections] ON");
+                await _db.SaveChangesAsync();
+                await _db.Database.ExecuteSqlCommandAsync("SET IDENTITY_INSERT [dbo].[Sections] OFF");
 
                 
                 transaction.Commit();
@@ -48,14 +42,36 @@ namespace WebStore.Data
             
             using (var transaction = _db.Database.BeginTransaction())
             {
-                _db.Products.AddRange(TestData.Products);
+                await _db.Brands.AddRangeAsync(TestData.Brands);
 
-                _db.Database.ExecuteSqlCommand("SET IDENTITY_INSERT [dbo].[Products] ON");
-                _db.SaveChanges();
-                _db.Database.ExecuteSqlCommand("SET IDENTITY_INSERT [dbo].[Products] OFF");
+                await _db.Database.ExecuteSqlCommandAsync("SET IDENTITY_INSERT [dbo].[Brands] ON");
+                await _db.SaveChangesAsync();
+                await _db.Database.ExecuteSqlCommandAsync("SET IDENTITY_INSERT [dbo].[Brands] OFF");
 
                 
                 transaction.Commit();
+            }
+            
+            using (var transaction = _db.Database.BeginTransaction())
+            {
+                await _db.Products.AddRangeAsync(TestData.Products);
+
+                await _db.Database.ExecuteSqlCommandAsync("SET IDENTITY_INSERT [dbo].[Products] ON");
+                await _db.SaveChangesAsync();
+                await _db.Database.ExecuteSqlCommandAsync("SET IDENTITY_INSERT [dbo].[Products] OFF");
+
+                
+                transaction.Commit();
+            }
+
+            await InitializeIdentityAsync();
+        }
+
+        private async Task InitializeIdentityAsync()
+        {
+            if (!await _roleManager.RoleExistsAsync(User.RoleUSer))
+            {
+                _roleManager.CreateAsync(new IdentityRole(User.RoleUSer))
             }
         }
     }
