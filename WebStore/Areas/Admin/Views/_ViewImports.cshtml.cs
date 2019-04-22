c043acd25e1ea50f85fcb6a616d8929da92f086b
ap-konovalov <sanches-zi@mail.ru>
2019-04-22 19:03:38 +0300

    Lesson 8. Admin layout add

---
+++ WebStore/Areas/Admin/Views/_ViewImports.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebStore.Views
+{
+    public class _ViewImports : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
---
+++ WebStore/Areas/Admin/Views/_ViewStart.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebStore.Views
+{
+    public class _ViewStart : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
