60de7571ce9339678ca6c5b17bd9f5908c0fbea8
ap-konovalov <sanches-zi@mail.ru>
2019-04-23 08:51:03 +0300

    Lesson 8. Done

--- WebStore/Controllers/ProfileController.cs
+++ WebStore/Controllers/ProfileController.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using WebStore.Infrastructure.Interfaces.WebStore.Infrastructure.Interfaces;
@@ -8,23 +9,30 @@ namespace WebStore.Controllers
     public class ProfileController : Controller
     {
         private readonly IOrderService _orderService;
-        public ProfileController(IOrderService orderService )
+
+        public ProfileController(IOrderService orderService)
         {
             _orderService = orderService;
         }
-        
+
         [Authorize]
         public IActionResult Index()
         {
             return
-            View();
+                View();
         }
 
         public IActionResult Orders()
         {
-            var orders =_orderService.GetUserOrders()
-            
-            return View();
+            var orders = _orderService.GetUserOrders(User.Identity.Name);
+
+            return View(orders.Select(order => new UserOrderViewModel
+            {
+                Name = order.Name,
+                Address = order.Address,
+                Phone = order.Phone,
+                TotalSum = order.OrderItems.Sum(o => o.Quantity * o.Price)
+            }));
         }
     }
 }
